0922529aae21d638981a530fc9a27e8778926f45
Phuong22122003 <hnguyenphuong09@gmail.com>
2024-07-21 13:36:50 +0700

    add some method to update quantity, price, description

--- src/main/java/com/webbanhang/webbanhang/Service/ProductService.java
+++ src/main/java/com/webbanhang/webbanhang/Service/ProductService.java
@@ -2,13 +2,17 @@ package com.webbanhang.webbanhang.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.webbanhang.webbanhang.Dto.ClientStockDto;
 import com.webbanhang.webbanhang.Dto.ProductInfoDto;
 import com.webbanhang.webbanhang.Entity.PRODUCT;
 import com.webbanhang.webbanhang.Repository.ProductRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ProductService {
@@ -45,5 +49,39 @@ public class ProductService {
         catch(Exception ex){
             return null;
         }
+    }
+    public List<ClientStockDto> getClientStock(Integer clientId){
+        try{
+            List<ClientStockDto> listClientStock = new ArrayList<>();
+            productRepository.getClientStock(clientId).forEach(item->{
+                listClientStock.add(new ClientStockDto(item.getProductId(),item.getImagePath(),item.getName(),item.getPrice(),item.getQuantity()));
+            });
+            return listClientStock;
+        }
+        catch(Exception ex){
+            return null;
+        }
+    }
+    @Transactional
+    public void updateQuantity(String productId ,Integer quantity){
+        productRepository.updateQuantity(quantity, productId);
+    }
+    @Transactional
+    public void updatePrice(String productId, Integer price){
+        productRepository.updatePrice(price, productId);
+    }
+    @Transactional
+    public void updateDescription(String productId, String description){
+
+        productRepository.updateDescription(description, productId);
+    }
+    @Transactional
+    public void updateName(String productId, String name){
+        productRepository.updateName(productId,name);
+    }
+    @Transactional
+    public void addProduct(PRODUCT product)
+    {
+        productRepository.save(product);
     }
 }
