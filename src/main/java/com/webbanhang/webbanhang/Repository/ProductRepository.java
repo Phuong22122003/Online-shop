5ce7a3aad2d1400078b8157e296eea2fe0cfbffa
Phuong22122003 <hnguyenphuong09@gmail.com>
2024-07-21 13:31:35 +0700

    add some method for updating product such as quantity, price ...

--- src/main/java/com/webbanhang/webbanhang/Repository/ProductRepository.java
+++ src/main/java/com/webbanhang/webbanhang/Repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.webbanhang.webbanhang.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -16,4 +17,21 @@ public interface ProductRepository extends JpaRepository<PRODUCT,String>{
     public PRODUCT findByProductId(@Param("productId") String productId); 
     @Query("FROM PRODUCT WHERE name LIKE %:keyword%")
     public List<PRODUCT> findProductBykeyword(@Param("keyword") String keyword);
+    @Query("FROM PRODUCT WHERE client.id =:clientId")
+    public List<PRODUCT> getClientStock(@Param("clientId") Integer clientId);
+    
+    @Modifying
+    @Query("UPDATE PRODUCT SET price =:price WHERE productId =:productId")
+    public void updatePrice(@Param("price") Integer price,@Param("productId") String productId);
+
+    @Modifying
+    @Query("UPDATE PRODUCT SET quantity = :quantity WHERE productId =:productId")
+    public void updateQuantity(@Param("quantity") Integer quantity,@Param("productId")String productId);
+
+    @Modifying
+    @Query("UPDATE PRODUCT  SET description = :description WHERE productId =:productId")
+    public void updateDescription(@Param("description") String description, @Param("productId") String productId);
+    @Modifying
+    @Query("UPDATE PRODUCT  SET name = :name WHERE productId =:productId")
+    public void updateName(@Param("name") String name, @Param("productId") String productId);
 }
