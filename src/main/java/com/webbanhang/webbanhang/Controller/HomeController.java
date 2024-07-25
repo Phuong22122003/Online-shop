bd4775bbf84a02889749128abdd60acab31dbea2
Phuong22122003 <hnguyenphuong09@gmail.com>
2024-07-25 13:50:33 +0700

    Moving logout, client stock, add,edit product to their own controller

--- src/main/java/com/webbanhang/webbanhang/Controller/HomeController.java
+++ src/main/java/com/webbanhang/webbanhang/Controller/HomeController.java
@@ -1,23 +1,12 @@
 package com.webbanhang.webbanhang.Controller;
 
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.webbanhang.webbanhang.Entity.PRODUCT;
 import com.webbanhang.webbanhang.Service.ProductService;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 
@@ -27,7 +16,9 @@ public class HomeController {
     @Autowired private ProductService productService;
     @GetMapping("/home")
     public String home(Model model,HttpSession session){
-        model.addAttribute("role", session.getAttribute("role").toString());
+        if(session.getAttribute("role")!=null)
+            model.addAttribute("role", session.getAttribute("role").toString());
+        else model.addAttribute("role","CUSTOMER");
         return "home";
     }   
     @GetMapping("/product-detail/{productId}")
@@ -55,28 +46,6 @@ public class HomeController {
     public String customerPuschase(){
         return "purchase";
     }
-    @GetMapping("/logout")
-    public String logout(HttpServletRequest request, HttpServletResponse response){
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null) {
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-        }
-        return "redirect:/login?logout"; // Chuyển hướng đến trang login sau khi logout
-    }
-    @GetMapping("/client-stock")
-    public String clientStock(){
-        return "client-stock";
-    }
-    @GetMapping("/edit-product/{productId}")
-    public String editProduct(@PathVariable("productId") String productId,Model model){
-        PRODUCT product = productService.getDetailProduct(productId);
-        model.addAttribute("product", product);
-        return "edit-stock";
-    }
-    @GetMapping("/add-product")
-    public String editProduct(Model model){
-        PRODUCT product = new PRODUCT();
-        model.addAttribute("product", product);
-        return "add-product";
-    }
+
+
 }
